3e8677ebb04dcb7d2e5da1ce567820100ff0570e
SantiP13 <s.penunuri@hotmail.com>
2024-05-29 14:24:20 -0600

    Third Commit

--- launch/launch_delta.py
+++ launch/launch_delta.py
@@ -1,9 +1,10 @@
 import os
-import tempfile
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 import xacro
@@ -13,31 +14,37 @@ def generate_launch_description():
     bringup_dir = get_package_share_directory('my_second_delta')
     urdf_path = os.path.join(bringup_dir, 'urdf', 'delta.urdf.xacro')
     robot_raw_description = xacro.process_file(urdf_path).toxml()
-
-    # Write the URDF to a temporary file
-    urdf_file = tempfile.NamedTemporaryFile(delete=False)
-    urdf_file.write(robot_raw_description.encode())
-    urdf_file.close()
+    sdf_path = os.path.join(bringup_dir, 'urdf', 'delta_pro.sdf')
 
     return LaunchDescription([
         Node(
-            package='gazebo_ros', 
+            package='rviz2', 
+            executable='rviz2', 
+            arguments=['-d',
+                       'install/my_second_delta/share/my_second_delta/launch_delta.rviz',
+                       '-d' + os.path.join(bringup_dir, 'config', 'config_file.rviz')],
+            output='screen',
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        ),
+        Node(
+            package='gazebo_ros',
             executable='spawn_entity.py',
-            arguments=['-entity', 'delta', '-file', urdf_file.name],
-            output='screen'
+            arguments=['-entity', 'delta', '-topic', 'robot_description'],
+            output='screen',
         ),
-
-
         Node(
-            package='robot_state_publisher', executable='robot_state_publisher',
-            output='screen'
+            package='robot_state_publisher', 
+            executable='robot_state_publisher', 
+            output='screen',
+            parameters=[{'robot_description': robot_raw_description}],
         ),
-
-        Node(
-            package='rviz2', executable='rviz2',
-            arguments=['-d', os.path.join(bringup_dir + '/config/config_file.rviz')],
-            output='screen'
-        ),
-
-        ])
-
+        # Node(
+        #     name='joint_state_publisher',
+        #     package='joint_state_publisher',
+        #     executable='joint_state_publisher',
+        #     parameters=[{'robot_description': robot_raw_description}]
+        # ),
+    ])
